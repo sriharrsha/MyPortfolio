ba0430c6289b286e2fed9b514aa2ba5c10923364
sriharrsha <sriharrsha@live.com>
2015-11-22 21:37:59 +0530

    feat:Changed the Portfolio App according to November Cohort
    Modified the buttons and added new buttons in accordance with november cohort app requirements.From Spotify to Popular Movies.

--- app/src/main/java/com/orange/sriharrsha/myportfolio/MainActivity.java
+++ app/src/main/java/com/orange/sriharrsha/myportfolio/MainActivity.java
@@ -20,20 +20,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @OnClick(R.id.spotify_app_button)
-    public void spotifyClicked(View view) {
-        Toast.makeText(getApplicationContext(), "This button will launch Spotify Streamer", Toast.LENGTH_SHORT).show();
+    @OnClick(R.id.popular_movies1_app_button)
+    public void popular1Clicked(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch Popular Movies I", Toast.LENGTH_SHORT).show();
     }
 
 
-    @OnClick(R.id.scores_app_button)
-    public void scoresClicked(View view) {
-        Toast.makeText(getApplicationContext(), "This button will launch Scores App", Toast.LENGTH_SHORT).show();
+    @OnClick(R.id.popular_movies2_app_button)
+    public void popular2Clicked(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch Popular Movies II", Toast.LENGTH_SHORT).show();
     }
 
-    @OnClick(R.id.library_app_button)
-    public void libraryClicked(View view) {
-        Toast.makeText(getApplicationContext(), "This button will launch Library App", Toast.LENGTH_SHORT).show();
+    @OnClick(R.id.super_duo_app_button)
+    public void superDuoClicked(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch Super Duo App", Toast.LENGTH_SHORT).show();
     }
 
     @OnClick(R.id.build_it_bigger_app_button)
@@ -42,14 +42,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @OnClick(R.id.xyz_reader_button)
-    public void xyzClicked(View view) {
-        Toast.makeText(getApplicationContext(), "This button will launch XYZ Reader", Toast.LENGTH_SHORT).show();
+    @OnClick(R.id.material_app_button)
+    public void materialClicked(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch Make it Material App", Toast.LENGTH_SHORT).show();
     }
 
-    @OnClick(R.id.capstone_app_button)
-    public void capstoneClicked(View view) {
-        Toast.makeText(getApplicationContext(), "This button will launch my capstone App", Toast.LENGTH_SHORT).show();
+    @OnClick(R.id.ubiquitous_app_button)
+    public void ubiquitousClicked(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch Go Ubiquitous App", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.capstone1_app_button)
+    public void capstone1Clicked(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch my Capstone I App", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.capstone2_app_button)
+    public void capstone2Clicked(View view) {
+        Toast.makeText(getApplicationContext(), "This button will launch my Capstone II App", Toast.LENGTH_SHORT).show();
     }
 
     @Override
